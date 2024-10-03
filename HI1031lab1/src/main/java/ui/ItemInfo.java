52d97e5a8ad97429892aa84178821a446fae508a
David42134 <102736029+David42134@users.noreply.github.com>
2024-10-03 15:45:41 +0200

    Update ItemInfo.java

--- HI1031lab1/src/main/java/ui/ItemInfo.java
+++ HI1031lab1/src/main/java/ui/ItemInfo.java
@@ -1,12 +1,18 @@
 package ui;
 
 public class ItemInfo {
+    private float price;
     private String name;
     private String description;
 
-    public ItemInfo(String name, String description) {
+    public ItemInfo(float price, String name, String description) {
+        this.price = price;
         this.name = name;
         this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
     }
 
     public String getName() {
