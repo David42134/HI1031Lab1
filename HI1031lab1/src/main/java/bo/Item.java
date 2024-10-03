d79bc3eac1728e44b400c5b63b7cd7f4c679211e
David42134 <102736029+David42134@users.noreply.github.com>
2024-10-03 15:44:36 +0200

    Update Item.java

--- HI1031lab1/src/main/java/bo/Item.java
+++ HI1031lab1/src/main/java/bo/Item.java
@@ -7,16 +7,22 @@ import java.util.Collection;
 public class Item {
     private String name;
     private String descr;
+    private float price;
     private int id;
 
     static public Collection searchItems(String group){
         return ItemDB.searchItems(group);
     }
 
-    protected Item(int id, String name, String descr){
+    protected Item(int id,float price, String name, String descr){
         this.id = id;
+        this.price = price;
         this.name = name;
         this.descr = descr;
+    }
+
+    public float getPrice() {
+        return this.price;
     }
 
     public String getName(){
